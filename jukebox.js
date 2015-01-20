74ac491e2b5555513feb223d31b791b1fccec377
winifredxu <winnielandau@gmail.com>
2015-01-19 22:24:58 -0800

    JukeBox perpetually prompt user for a song string, unless they enter ''.

---
+++ jukebox.js
@@ -0,0 +1,44 @@
+var parseNote = function(noteStr) {
+	var i =0;
+	var tmpArr = [];
+	var indNote = noteStr.split('*');
+	if (!indNote[1]) {
+		indNote[1] = 1;
+	}
+	while (indNote[i]) {
+		tmpArr.push(indNote[i]);
+		i +=1;
+	}
+//console.log(tmpArr);
+	return(tmpArr);
+};
+
+var parseSong = function(songStr) {
+	var noteArr = songStr.split(" ");
+	var tmp2Arr = [];
+
+	for (var i=0; i <noteArr.length; i += 1) {
+		var noteObj = new Object;
+		var tmp1Arr = [];		
+
+		tmp1Arr = parseNote(noteArr[i]);
+		noteObj["pitch"] = tmp1Arr[0];
+		noteObj["beats"] = tmp1Arr[1];	
+		tmp2Arr.push(noteObj);
+	}
+console.log(tmp2Arr);
+	return tmp2Arr;
+};
+
+//playSong(parseSong("C#*5 A*1 D*2"), 400, alert("finished!"));
+//playSong( [{pitch: 'A', beats: 1}, {pitch: 'C#', beats: 2}, {pitch: 'D', beats: 4}], 400, myAlert);
+
+var getSong = function() {
+	var songStr = prompt("Please enter a song string");
+	if (songStr=="") {
+		return;
+	}
+	playSong(parseSong(songStr), 400, getSong );
+};
+
+getSong();
